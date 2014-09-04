e4875608e522eb3895e7ef0de91533a6558f3c9e
j.faassen <j.faassen@linkorb.com>
2014-09-04 22:27:52 +0200

    Adds RestDatasource and DatasourceInterface

---
+++ src/Datasource/DatasourceInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace FrontController\Datasource;
+
+interface DatasourceInterface
+{
+    public function getData($path);
+}
---
+++ src/Datasource/RestDatasource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace FrontController\Datasource;
+
+class RestDatasource implements DatasourceInterface
+{
+    public function __construct($config = array())
+    {
+        $this->username = $config['username'];
+        $this->password = $config['password'];
+        $this->baseurl = $config['baseurl'];
+        $this->client = new \GuzzleHttp\Client();
+
+    }
+    
+    public function getData($parameters = array())
+    {
+        $path = $parameters['path'];
+        $url = $this->baseurl . $path;
+        $res = $this->client->get($url, array());
+        
+        return json_decode($res->getBody(), true);
+    }
+}
