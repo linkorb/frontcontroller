30cdb6542d53d9c4e9abdabad7084a5cfc726e81
j.faassen <j.faassen@linkorb.com>
2018-01-09 16:48:31 +0100

    Updates to datasources

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -25,6 +25,88 @@ $app->before(function (Request $request, Application $app) {
         return $p->text($value);
     });
     $app['twig']->addFilter($filter);
+
+    $filter = new Twig_SimpleFilter('defined', function ($value) use ($app, $request) {
+        foreach ($request->attributes->all() as $k => $v) {
+            if (is_string($v)) {
+                $value = str_replace('{' . $k . '}', $v, $value);
+            }
+        }
+        if ($request->attributes->has('variables')) {
+            $variables = $request->attributes->get('variables');
+            return isset($variables[$value]);
+        }
+        return false;
+    });
+    $app['twig']->addFilter($filter);
+
+
+    $filter = new Twig_SimpleFilter('variable', function ($value) use ($app, $request) {
+        foreach ($request->attributes->all() as $k => $v) {
+            if (is_string($v)) {
+                $value = str_replace('{' . $k . '}', $v, $value);
+            }
+        }
+        if ($request->attributes->has('variables')) {
+            $variables = $request->attributes->get('variables');
+            if (isset($variables[$value])) {
+                return $variables[$value];
+            } else {
+                return '?' . $value . '?';
+            }
+        }
+        return '?no-variables?';
+    });
+    $app['twig']->addFilter($filter);
+
+    $ds = $app['frontcontroller.datasource'];
+    $data = $request->attributes->get('data');
+    if ($data) {
+        foreach ($data as $name => $config) {
+            $path = $config['path'];
+
+            // Process `path` with url parameters
+            foreach ($request->attributes->all() as $key => $value) {
+                if (is_string($value)) {
+                    $path = str_replace('{' . $key . '}', $value, $path);
+                }
+            }
+            // fetch data from data-source
+            $values = $ds->getData(array('path' => $path));
+
+            // Optionally use offset in data
+            if (isset($config['offset'])) {
+                $values = $values[$config['offset']];
+            }
+
+            // Optionally re-key
+            if (isset($config['key'])) {
+                $res = [];
+                foreach ($values as $key => $value) {
+                    $newKey = $value[$config['key']];
+                    if (isset($config['value'])) {
+                        $value = $value[$config['value']];
+                    }
+                    $res[$newKey] = $value;
+                }
+                $values = $res;
+            }
+
+            if (isset($config['mode'])) {
+                switch ($config['mode']) {
+                    case 'first':
+                        $values = $values[0];
+                        break;
+                    case 'last':
+                        break;
+                    case 'all':
+                    default:
+                        break;
+                }
+            }
+            $request->attributes->set($name, $values);
+        }
+    }
 });
 
 $controllerresolver = new \FrontController\ControllerResolver($app, null);
--- src/Module/TwigTemplate/TwigTemplateModule.php
+++ src/Module/TwigTemplate/TwigTemplateModule.php
@@ -8,54 +8,11 @@ use FrontController\Core\ModuleInterface;
 
 class TwigTemplateModule implements ModuleInterface
 {
-    public function handle(Application $app, Request $request, $template = null, $data = [])
+    public function handle(Application $app, Request $request, $template = null)
     {
         $input = array();
-        $ds = $app['frontcontroller.datasource'];
-
-        foreach ($data as $name => $config) {
-            $path = $config['path'];
-
-            // Process `path` with url parameters
-            foreach ($request->attributes->all() as $key => $value) {
-                if (is_string($value)) {
-                    $path = str_replace('{' . $key . '}', $value, $path);
-                }
-            }
-            // fetch data from data-source
-            $values = $ds->getData(array('path' => $path));
-
-            // Optionally use offset in data
-            if (isset($config['offset'])) {
-                $values = $values[$config['offset']];
-            }
-
-            // Optionally re-key
-            if (isset($config['key'])) {
-                $res = [];
-                foreach ($values as $key => $value) {
-                    $newKey = $value[$config['key']];
-                    if (isset($config['value'])) {
-                        $value = $value[$config['value']];
-                    }
-                    $res[$newKey] = $value;
-                }
-                $values = $res;
-            }
-
-            if (isset($config['mode'])) {
-                switch ($config['mode']) {
-                    case 'first':
-                        $values = $values[0];
-                        break;
-                    case 'last':
-                        break;
-                    case 'all':
-                    default:
-                        break;
-                }
-            }
-            $input[$name] = $values;
+        foreach ($request->attributes->all() as $key=>$value) {
+            $input[$key] = $value;
         }
 
         // print_r($input);exit();
